24d2a5465bdd249a20c61f3485897cd91fa82438
nicsiv <nicoleusivek@gmail.com>
2021-01-05 11:29:22 -0600

    controller update

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,16 +1,5 @@
 class IngredientsController < ApplicationController
   
-  def index
-    ingredients = Ingredient.all
-    render json: ingredients, only: [:id, :name, :category, :image]
-  end
-
-  def show
-    ingredient = Ingredient.find_by(id: params[:id])
-    render json: ingredient.slice(:id, :name, :category, :image)
-  else
-    render json: { message: 'Ingredient not found' }
-  end
     def index
         ingredients = Ingredient.all
         render json: ingredients
